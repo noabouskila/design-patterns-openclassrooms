80cdd10138433a93fad667f0f85c2ce89028b12a
John Doe <noabouskil@gmail.com>
2023-05-01 14:13:03 +0200

    part 2 quiz quest3

--- js/App.js
+++ js/App.js
@@ -4,6 +4,28 @@ const fetchData = () => fetch('/data/data.json')
 
 
 class Movie {
+    constructor(data) {
+        this._id = data.id
+        this._title = data.title
+        this._img = data.img
+        this._rating = data.rating
+    }
+ 
+    get id() {
+        return this._id
+    }
+ 
+    get title() {
+        return this._title
+    }
+ 
+    get img() {
+        return this._img
+    }
+ 
+    get rating() {
+        return this._rating
+    }
 }
 
 
